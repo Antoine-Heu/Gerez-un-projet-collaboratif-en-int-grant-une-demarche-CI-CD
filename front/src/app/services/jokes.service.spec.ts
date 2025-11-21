2753ebe10f100caebb4c89d72a3fa94154a0d1a5
Antoine Heu <aheu@whispeak.io>
2025-11-21 16:41:09 +0100

    add a comment

--- front/src/app/services/jokes.service.spec.ts
+++ front/src/app/services/jokes.service.spec.ts
@@ -14,6 +14,7 @@ describe('JokesService', () => {
       service = TestBed.get(JokesService);
   });
 
+  // my awesome comment
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
